2d729da9c9b4281a73bf6af19f73507225babc38
mostafa besher <m.besher@neoxero.com>
2025-01-22 20:51:05 +0200

    upload tree traversal breadthfirst and depthfirst  implementation

--- section22-23-binary-search-tree/app.js
+++ section22-23-binary-search-tree/app.js
@@ -69,17 +69,107 @@ class BinarySearchTree {
     }
     return isFound;
   }
+
+  breadthFirstSearch() {
+    if (!this.root) return undefined;
+
+    const queue = [];
+    const visited = [];
+    let curNode;
+
+    queue.push(this.root);
+    while (queue.length > 0) {
+      curNode = queue.shift();
+      visited.push(curNode.val);
+      if (curNode.left) {
+        queue.push(curNode.left);
+      }
+
+      if (curNode.right) {
+        queue.push(curNode.right);
+      }
+    }
+
+    return visited;
+  }
+
+  depthFirstPreorder() {
+    const visited = [];
+    function traverse(node) {
+      // base case
+      if (node === null) return visited;
+
+      // recursive case
+      visited.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstPostorder() {
+    const visited = [];
+    function traverse(node) {
+      // base case
+      // if (node === null) return visited;
+
+      // recursive case
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node.val);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstInorder() {
+    const visited = [];
+    function traverse(node) {
+      // base case
+      // if (node === null) return visited;
+
+      // recursive case
+      if (node.left) traverse(node.left);
+      visited.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
 }
 
 const binaryTree = new BinarySearchTree();
 /////////////////insert///////////////
+// console.log(binaryTree.insert(10));
+// console.log(binaryTree.insert(15));
+// console.log(binaryTree.insert(3));
+// console.log(binaryTree.insert(20));
+// console.log(binaryTree.insert(9));
+// console.log(binaryTree.insert(5));
+
 console.log(binaryTree.insert(10));
+console.log(binaryTree.insert(6));
 console.log(binaryTree.insert(15));
-console.log(binaryTree.insert(3));
 console.log(binaryTree.insert(20));
-console.log(binaryTree.insert(9));
-console.log(binaryTree.insert(5));
+console.log(binaryTree.insert(3));
+console.log(binaryTree.insert(8));
+
 /////////////////find///////////////
-console.log(binaryTree.find(9));
-console.log(binaryTree.find(19));
-console.log(binaryTree.find(3));
+// console.log(binaryTree.find(9));
+// console.log(binaryTree.find(19));
+// console.log(binaryTree.find(3));
+
+/////////////////Breadth First Search///////////////
+console.log(binaryTree.breadthFirstSearch());
+
+/////////////////Depth First Search(preOrder)///////////////
+console.log(binaryTree.depthFirstPreorder());
+
+/////////////////Depth First Search(postOrder)///////////////
+console.log(binaryTree.depthFirstPostorder());
+//
+/////////////////Depth First Search(inOrder)///////////////
+console.log(binaryTree.depthFirstInorder());
